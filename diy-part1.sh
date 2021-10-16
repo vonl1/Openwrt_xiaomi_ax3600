e77800faf01cc03f410e95bfe1c1052baf9ca946
VonLee <40910092+vonl1@users.noreply.github.com>
2021-10-16 21:33:30 +0800

    change to boos4721's package

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,11 +7,11 @@
 #
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Description: OpenWrt DIY script part 1 (Before https://github.com/kenzok8/openwrt-packagesUpdate feeds)
 #
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source 
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git Boos4721 https://github.com/Boos4721/OpenWrt-Packages' >>feeds.conf.default
